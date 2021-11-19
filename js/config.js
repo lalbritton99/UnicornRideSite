76baff0b707b3630eac50a6f1cd300464c844956
Logan Albritton <logansspamalishous@gmail.com>
2021-11-19 13:53:18 -0600

    add api invoke

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-2_5vtJQZfeE',
         userPoolClientId: '5jvkslc9898du2645u5sajq3c3',
-        region: 'us-east-2' // e.g. us-east-2
+        region: 'us-east-2'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://gcrzv049hd.execute-api.us-east-2.amazonaws.com/prod'
     }
 };
